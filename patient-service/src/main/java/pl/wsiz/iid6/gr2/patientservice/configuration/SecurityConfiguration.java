2071981cde4d23804c5023481d00101d00136e59
Kamilgolda <kamil.goldaat@gmail.com>
2021-07-13 12:54:15 +0200

    Links in Navbar

--- patient-service/src/main/java/pl/wsiz/iid6/gr2/patientservice/configuration/SecurityConfiguration.java
+++ patient-service/src/main/java/pl/wsiz/iid6/gr2/patientservice/configuration/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .antMatchers("/").permitAll()
                 .antMatchers("/home").permitAll()
                 .antMatchers("/registration").permitAll()
+                .antMatchers("/patient").hasAnyRole("PACJENT", "LEKARZ", "ADMIN")
                 .antMatchers("/patient/lnames**").hasRole("ADMIN")
                 .and()
                 .formLogin()
